a295aa202ab7c01c30714d5f3cec9d5c11c1ba54
King Kong <king-kong@gmail.com>
2022-11-01 11:59:27 -0700

    test and route for zodiac id

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,11 +3,22 @@ const app = require('../lib/app');
 const { zodiacs } = require('../lib/zodiac-data');
 
 describe('zodiacs routes', () => {
-  it('/zodiacs should return list of zodiacs', async () => {
-    const resp = await request(app).get('/zodiacs');
+  it('/zodiacs should return list of ids/zodiacs', async () => {
+    const resp = await request(app).get('/zodiac');
     const expected = zodiacs.map((zodiac) => {
       return { id: zodiac.id, name: zodiac.name };
     });
     expect(resp.body).toEqual(expected);
+  });
+
+  it('zodiac:id should return infomration about the sign.', async () => {
+    const resp = await request(app).get('/zodiac/1');
+    const zOne = {
+      id: '1',
+      name: 'aquarius',
+      dates: 'Jan 21 - Feb 19',
+      symbol: 'Water Bearer',
+    };
+    expect(resp.body).toEqual(zOne);
   });
 });
