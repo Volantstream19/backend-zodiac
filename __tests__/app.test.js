190b7e05b39f3817aea7900be061eba8dbffcc59
King Kong <king-kong@gmail.com>
2022-11-01 10:53:55 -0700

    direct to home page with both lists

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,6 +1,8 @@
 const request = require('supertest');
 const app = require('../lib/app');
 
+const { zodiacs } = require('../lib/zodiac-data');
+
 describe('example test - you should probably update me', () => {
   it('home route should return hello world', async () => {
     const resp = await request(app).get('/');
